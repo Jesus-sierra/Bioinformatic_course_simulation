85acf8179a1c461a24994035f9aefc43c6a68189
Jesus-sierra <168840073+Jesus-sierra@users.noreply.github.com>
2024-05-18 19:44:04 -0300

    Add files via upload

---
+++ trabalhos/rosalind2.py
@@ -0,0 +1,8 @@
+#rosalind2.py
+#This script processes a basic arithmetic operation
+#Author: Jesus Sierra
+#Date: 18/05/2024
+
+def ros2(a,b):
+    c=(a**2)+(b**2)
+    print("O quadrado da hipotenusa é:", c)
